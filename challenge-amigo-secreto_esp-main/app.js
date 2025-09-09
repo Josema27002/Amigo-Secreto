a2c3cd6b9bea20b4ba854280e14e8b01586ed84d
Josema Parra <parragarcia7olimpia@gmail.com>
2025-09-09 00:51:31 -0300

    Creamos la funcion del array

--- challenge-amigo-secreto_esp-main/app.js
+++ challenge-amigo-secreto_esp-main/app.js
@@ -1,1 +1,20 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigos() {
+    let input = document.getElementById("amigo");
+    let nombre = input.value.trim();
+
+// Validamos el nombre para que no este vacio
+if (nombre === "") {
+    alert("Por favor, ingresa un nombre valido para agregar en el sorteo.");
+    return;
+}
+// Para guardar los nombres en el array
+amigos.push(nombre);
+
+//Limpiamos el input
+input.value = "";
+
+// Para mostrar la lista actualizada
+mostrarLista();
+}
